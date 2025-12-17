9cf971e299497e085dd7c7d2fcf57d637ca3101b
emileletueur <galina.sebastien@gmail.com>
2025-12-17 17:40:04 +0100

    Update fpv_sound_logger.py

--- src/lib/fpv_sound_logger.py
+++ src/lib/fpv_sound_logger.py
@@ -1,3 +1,12 @@
+# Copyright (C) 2025 GALINA Sébastien <galina.sebastien@gmail.om>
+# 
+# This file is part of FPVSoundLogger.
+#
+# MonProjet is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
 import os, time
 import uasyncio as asyncio
 from machine import Pin, I2S, SPI
