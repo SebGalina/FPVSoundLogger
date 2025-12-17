9b055332479b5ddfe2e68745e9d4e96954578387
emileletueur <galina.sebastien@gmail.com>
2025-12-17 17:40:16 +0100

    Update fpv_sound_logger.py

--- src/lib/fpv_sound_logger.py
+++ src/lib/fpv_sound_logger.py
@@ -2,7 +2,7 @@
 # 
 # This file is part of FPVSoundLogger.
 #
-# MonProjet is free software: you can redistribute it and/or modify
+# FPVSoundLogger is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
