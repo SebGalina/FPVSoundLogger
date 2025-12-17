6d7181bdd2a0b5178e6d88f4290014c923c79f3c
emileletueur <galina.sebastien@gmail.com>
2025-12-17 17:39:29 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
+# Copyright (C) 2025 GALINA Sébastien <galina.sebastien@gmail.om>
+# 
+# This file is part of MonProjet.
+#
+# MonProjet is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
 import uasyncio as asyncio
 from lib.fpv_sound_logger import FpvSoundLogger
 
